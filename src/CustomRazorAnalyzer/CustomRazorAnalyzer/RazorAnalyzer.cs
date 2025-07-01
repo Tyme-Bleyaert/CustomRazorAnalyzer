773428bd5d17832f9be15e51d6350e9296957b0c
Tyme Bleyaert <Tyme.Blyaert@kenze.be>
2025-07-01 08:44:46 +0200

    Some cleanup, use addition file action instead.

--- src/CustomRazorAnalyzer/CustomRazorAnalyzer/RazorAnalyzer.cs
+++ src/CustomRazorAnalyzer/CustomRazorAnalyzer/RazorAnalyzer.cs
@@ -26,87 +26,81 @@ public class RazorAnalyzer : DiagnosticAnalyzer
     {
         context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
         context.EnableConcurrentExecution();
-        context.RegisterCompilationStartAction(ctx =>
+        context.RegisterAdditionalFileAction(ctx =>
         {
             var diagnostics = ImmutableArray.CreateBuilder<Diagnostic>();
-            var additionalFiles = ctx.Options.AdditionalFiles;
-            foreach (var file in additionalFiles)
+            var file = ctx.AdditionalFile;
+
+            if (file.Path.EndsWith(".razor", StringComparison.OrdinalIgnoreCase))
             {
-                if (file.Path.EndsWith(".razor", StringComparison.OrdinalIgnoreCase))
+                var text = file.GetText(ctx.CancellationToken);
+                if (text == null)
+                    return;
+
+                var razorContent = text.ToString();
+                var commentPattern = @"@\*(.*?)\*@";
+                var commentMatches = Regex.Matches(razorContent, commentPattern);
+
+                // Precompute line start positions
+                string[] lines = razorContent.Split(["\r\n", "\n"], StringSplitOptions.None);
+                int[] lineStartPositions = new int[lines.Length];
+                int pos = 0;
+                for (int i = 0; i < lines.Length; i++)
                 {
-                    var text = file.GetText(ctx.CancellationToken);
-                    if (text == null)
-                        return;
+                    lineStartPositions[i] = pos;
+                    // +1 for '\n' or +2 for '\r\n'? To be safe, count exact length:
+                    pos += lines[i].Length + 1; // Assume '\n'; okay since splitting on both
 
+                    // But if original had \r\n, length of line + 2 might be needed
+                    // This approximation works if lines end in \n or \r\n consistently
+                }
 
-                    var razorContent = text.ToString();
-                    var commentPattern = @"@\*(.*?)\*@";
-                    var commentMatches = Regex.Matches(razorContent, commentPattern);
-
-                    // Precompute line start positions
-                    string[] lines = razorContent.Split(["\r\n", "\n"], StringSplitOptions.None);
-                    int[] lineStartPositions = new int[lines.Length];
-                    int pos = 0;
-                    for (int i = 0; i < lines.Length; i++)
+                foreach (Match commentMatch in commentMatches)
+                {
+                    int matchIndex = commentMatch.Index;
+                    // Find which line this index belongs to by binary search or linear scan
+                    int lineNumber = Array.FindLastIndex(lineStartPositions, start => start <= matchIndex);
+                    if (commentMatch.Success)
                     {
-                        lineStartPositions[i] = pos;
-                        // +1 for '\n' or +2 for '\r\n'? To be safe, count exact length:
-                        pos += lines[i].Length + 1; // Assume '\n'; okay since splitting on both
-
-                        // But if original had \r\n, length of line + 2 might be needed
-                        // This approximation works if lines end in \n or \r\n consistently
+                        string extracted = commentMatch.Groups[1].Value;
+                        //TODO add razor comment syntax to diagnostics as well.
                     }
+                }
 
-                    foreach (Match commentMatch in commentMatches)
+                // Simple regex to extract @code blocks
+                var matches = Regex.Matches(razorContent, @"@code\s*{([^}]*)}", RegexOptions.Singleline);
+
+                foreach (Match match in matches)
+                {
+                    int matchIndex = match.Index;
+                    // Find which line this index belongs to by binary search or linear scan
+                    int lineNumber = Array.FindLastIndex(lineStartPositions, start => start <= matchIndex);
+
+                    var codeContent = match.Groups[1].Value;
+
+                    var tree = CSharpSyntaxTree.ParseText(codeContent, path: file.Path);
+                    var root = tree.GetRoot(ctx.CancellationToken);
+
+                    var todos = root.DescendantTrivia().Where(t => t.IsKind(SyntaxKind.SingleLineCommentTrivia) && t.ToString().Contains("TODO"));
+
+                    foreach (var todo in todos)
                     {
-                        int matchIndex = commentMatch.Index;
-                        // Find which line this index belongs to by binary search or linear scan
-                        int lineNumber = Array.FindLastIndex(lineStartPositions, start => start <= matchIndex);
-                        if (commentMatch.Success)
-                        {
-                            string extracted = commentMatch.Groups[1].Value;
-                            //TODO add razor comment syntax to diagnostics as well.
-                        }
-                    }
+                        var lineSpan = todo.GetLocation().GetLineSpan();
+                        var fakeLinePos = new FileLinePositionSpan(
+                            lineSpan.Path,
+                            new LinePosition(lineSpan.StartLinePosition.Line - 1 + lineNumber, lineSpan.StartLinePosition.Character),
+                            new LinePosition(lineSpan.EndLinePosition.Line - 1 + lineNumber, lineSpan.EndLinePosition.Character)
+                        );
 
-                    // Simple regex to extract @code blocks
-                    var matches = Regex.Matches(razorContent, @"@code\s*{([^}]*)}", RegexOptions.Singleline);
-                    int parsecount = 0;
-                    foreach (Match match in matches)
-                    {
-                        int matchIndex = match.Index;
-                        // Find which line this index belongs to by binary search or linear scan
-                        int lineNumber = Array.FindLastIndex(lineStartPositions, start => start <= matchIndex);
-
-                        var codeContent = match.Groups[1].Value;
-
-                        var tree = CSharpSyntaxTree.ParseText(codeContent, path: file.Path + $"_parsed_codeblock{++parsecount}");
-                        var root = tree.GetRoot(ctx.CancellationToken);
-
-                        var todos = root.DescendantTrivia().Where(t => t.IsKind(SyntaxKind.SingleLineCommentTrivia) && t.ToString().Contains("TODO"));
-
-                        foreach (var todo in todos)
-                        {
-                            var lineSpan = todo.GetLocation().GetLineSpan();
-                            var fakeLinePos = new FileLinePositionSpan(
-                                lineSpan.Path,
-                                new LinePosition(lineSpan.StartLinePosition.Line - 1 + lineNumber, lineSpan.StartLinePosition.Character),
-                                new LinePosition(lineSpan.EndLinePosition.Line - 1 + lineNumber, lineSpan.EndLinePosition.Character)
-                            );
-
-                            var diagnostic = Diagnostic.Create(Rule, Location.Create(file.Path, todo.Span, fakeLinePos.Span));
-                            diagnostics.Add(diagnostic);
-                        }
+                        var diagnostic = Diagnostic.Create(Rule, Location.Create(file.Path, todo.Span, fakeLinePos.Span));
+                        diagnostics.Add(diagnostic);
                     }
                 }
             }
 
-            // Register an end action that reports the buffered diagnostics
-            ctx.RegisterCompilationEndAction(endContext =>
-            {
-                foreach (var diag in diagnostics)
-                    endContext.ReportDiagnostic(diag);
-            });
+            foreach (var diag in diagnostics)
+                ctx.ReportDiagnostic(diag);
+
         });
     }
 
